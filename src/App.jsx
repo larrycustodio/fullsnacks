9e74eca3b9d52d4570d4225b5600afba2ef01811
larrycustodio <custodioly@gmail.com>
2017-10-12 01:24:36 -0700

    initial adjustments to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,74 @@
 import React, { Component } from 'react';
 
-
 class App extends Component {
-    constructor(){
+    constructor() {
         super();
 
         this.state = {
-            lochnessMonster: 3.50
+            activeSelection: {
+                activechoice: '',
+                time: ''
+            },
+            options: [
+                {
+                    name: 'coffee',
+                    categories: ['coffee', 'coffeeroasteries'],
+                    selectionSrc: './assets/coffee.svg',
+                    spriteSrc: './assets/coffee_sprite.svg'
+                },
+                {
+                    name: 'donut',
+                    categories: ['donuts','bakeries'],
+                    selectionSrc: './assets/donut.svg',
+                    spriteSrc: './assets/donut_sprite.svg'
+                },
+                {
+                    name: 'brunch',
+                    categories: ['breakfast_brunch'],
+                    selectionSrc: './assets/breakfast.svg',
+                    spriteSrc: './assets/breakfast_sprite.svg'
+                }
+            ]
         }
     }
 
-    render(){
+    render() {
         return (
             <div className='container'>
-                Homepage
+                <nav className='nav'>
+                    Breakfast Finder
+                </nav>
+                <HomeOptions options={this.state.options} />
             </div>
         );
     }
 }
 
+function HomeOptions(props) {
+    return (
+        <section className='section-container'>
+            <h1 className='section-header'>Good morning!</h1>
+            <p className='section-subheader'>what do you feel like having today?</p>
+            <div className='section-selection'>
+                {props.options.map((breakfastItem, index) => {
+                    return (
+                        <BreakfastOption
+                            key={'breakfast-' + index}
+                            item={breakfastItem} />
+                    );
+                })}
+            </div>
+        </section>
+    );
+}
+
+function BreakfastOption(props) {
+    console.log(props.item.name);
+    return (
+        <div className='breakfast-option'>
+            <p>{props.item.name}</p>
+            <img src={props.item.selectionSrc} />
+        </div>
+    )
+}
 export default App;
