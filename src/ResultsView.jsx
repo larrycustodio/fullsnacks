b34b5563647e0e7513131b5ef3c90cd190cd309e
larrycustodio <custodioly@gmail.com>
2017-10-13 14:33:06 -0700

    Divided components

--- src/ResultsView.jsx
+++ src/ResultsView.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import LoadingContainer from './LoadingView';
 import axios from 'axios';
+import YelpResultsContainer from '/Results_Components/YelpResultsContainer';
+import MapContainer from '/Results_Components/MapContainer';
 
 class ResultsView extends Component {
     constructor(props) {
@@ -14,6 +16,7 @@ class ResultsView extends Component {
                 sort: '1',
             },
             yelpResults: {},
+            googleMapsResults: {}, 
             isSearchDone: false
         }
     }
@@ -43,7 +46,7 @@ class ResultsView extends Component {
             console.log('done!');
             return (
                     <div className='view-container view-search-results'>
-                        <ResultsContainer
+                        <YelpResultsContainer
                             businesses={this.state.yelpResults.businesses}
                             categories={this.props.categories}
                             onClick={this.onClickHandler} />
@@ -52,73 +55,6 @@ class ResultsView extends Component {
                 );
             }
     }
-}
-
-const ResultsContainer = (props) => {
-    return (
-        <div className='display-list-results'>
-            <div className='results-nearby'>
-                Found {props.businesses.length} places nearby!
-            </div>
-            <div className='results-list'>
-                {props.businesses.map((place,index) => {
-                    return (
-                        <BusinessList className='list'
-                        key={'list-'+index}
-                        businessInfo={place} />
-                    );
-                })}
-            </div>
-            <div className='toggle-cat'>
-                {props.categories.map((icon, index) => {
-                    return (
-                        <CategorySprite className='cat-icon'
-                            key={'sprite-' + index}
-                            index={index}
-                            onClick={props.onClick}
-                            icon={icon} />
-                    );
-                })}
-            </div>
-        </div>
-    );
-}
-const BusinessList = (props) => {
-    const info = props.businessInfo;
-    const backgroundImage = {
-        background: 'linear-gradient(rgba(87,87,87,0.61),rgba(87,87,87,0.61)), url(' + props.businessInfo.image_url + ')',
-        backgroundSize: 'cover',
-    }
-    const ratingStars = Math.trunc(info.rating) !== info.rating ? Math.trunc(info.rating) + '_half' : info.rating; 
-    return (
-        <div className='result-list-card' style={backgroundImage}>
-            <p className='list-name'>{info.name}</p>
-            <img className='list-rating'
-                src=  {'./assets/yelp_stars/small/small_' + ratingStars + '.png' } />
-            <p className='list-distance'>{'~'+ (info.distance * 0.622 * 0.001).toFixed(1) + ' miles away'}</p>
-        </div>
-    );
-}
-const CategorySprite = (props) => {
-    return (
-        <div className='cat-icon'
-            onClick={props.onClick}
-            data-category-value={props.index} >
-            <img className='cat-icon-svg'
-                data-category-value={props.index}
-                src={props.icon.spriteSrc} />
-            <p className='cat-icon-name'>
-                {props.icon.name.toUpperCase()}
-            </p>
-        </div>
-    );
-}
-const MapContainer = (props) => {
-    return (
-        <div className='display-map-results'>
-            Map Goes here!
-        </div>
-    )
 }
 
 export default ResultsView;
---
+++ src/Results_Components/MapContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MapContainer = (props) => {
+    return (
+        <div className='display-map-results'>
+            Map Goes here!
+        </div>
+    )
+}
+
+export default MapContainer;
