911a4f08ed5b09fb97841eb25b3d512713481f65
larrycustodio <larry@helloextend.com>
2019-11-09 12:14:18 -0800

    global styles + other head tag semantics

---
+++ src/GlobalStyles.tsx
@@ -0,0 +1,38 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  * {
+      box-sizing: border-box;
+  }
+  html {
+    line-height: 1.15;
+    -webkit-text-size-adjust: 100%;
+  }
+  body {
+    margin: 0;
+    min-height: 100vh;
+    font-family: Helvetica, Arial, sans-serif;
+  }
+  main {
+    display: block;
+  }
+  a {
+    background-color: transparent;
+    color: inherit;
+  }
+  button,
+  input,
+  optgroup,
+  select,
+  textarea {
+    font-family: inherit;
+    font-size: 100%;
+    line-height: 1.15;
+    margin: 0;
+  }
+  strong {
+    font-weight: 700;
+  }
+`
+
+export default GlobalStyle
--- src/Main.tsx
+++ src/Main.tsx
@@ -1,22 +1,26 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import GlobalStyle from './GlobalStyles'
 import Home from './pages/Home'
 import ClaimsConcepts from './pages/ClaimsConcepts'
 
 interface MainProps {}
 
 const Main: React.FC<MainProps> = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/claims-concepts">
-        <ClaimsConcepts />
-      </Route>
-      <Route exact path="/">
-        <Home />
-      </Route>
-    </Switch>
-  </Router>
+  <div>
+    <GlobalStyle />
+    <Router>
+      <Switch>
+        <Route exact path="/claims-concepts">
+          <ClaimsConcepts />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  </div>
 )
 
 ReactDOM.render(<Main />, document.getElementById('app'))
